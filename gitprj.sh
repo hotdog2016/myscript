7efd0802aac0aaa9984e30691d6b5c1964799bac
hotdog2016 <yujinxing2013@126.com>
2020-04-15 14:11:04 +0800

    2020-04-15 14:11:04

---
+++ gitprj.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+gitfile="autogit.sh"
+gitdir=".git"
+
+function autocommit {
+	cd $1 
+	echo "now dir is ${PWD}"
+	if [  -d "$gitdir" ]; then
+		echo "find .git dir"
+		if [  -f "$gitfile" ]; then
+				./$gitfile
+			echo "$PWD git OK!"
+			else
+				echo "$PWD can't find autogit.sh"
+				return	
+		fi
+	else
+		echo "$PWD can't find .git"
+		return	
+	fi
+}
+
+autocommit ~/workspace/C
+autocommit ~/workspace/myscript
+autocommit ~/.config
+autocommit ~/vimwiki
+
